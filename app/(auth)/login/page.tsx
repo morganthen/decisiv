7d9d60376e2e7752d566e2612c9d79e504713ab1
Morgan Then <morganthen@me.com>
2026-03-09 21:18:23 +0800

    fix: another attempt at fixing login page suspense error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -17,41 +17,40 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useActionState } from "react";
 
-export default function Login() {
+function LoginForm() {
   const [state, formAction, isPending] = useActionState(login, null);
   const searchParams = useSearchParams();
   const message = searchParams.get("message");
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="h-dvh bg-background flex flex-col justify-center">
-        <Card className="mx-10">
-          <CardHeader>
-            <CardTitle>Login to your account</CardTitle>
-            <CardDescription>
-              Enter your email and password below to login to your account
-            </CardDescription>
-            <CardAction>
-              <Button variant="link">
-                <Link href="/signup">Sign Up</Link>
-              </Button>
-            </CardAction>
-          </CardHeader>
-          <CardContent>
-            <form action={formAction}>
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                    name="email"
-                  />
-                </div>
-                <div className="grid gap-2">
-                  {/* <div className="flex items-center">
+    <div className="h-dvh bg-background flex flex-col justify-center">
+      <Card className="mx-10">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email and password below to login to your account
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">
+              <Link href="/signup">Sign Up</Link>
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form action={formAction}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  name="email"
+                />
+              </div>
+              <div className="grid gap-2">
+                {/* <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                   <a
                   href="#"
@@ -60,27 +59,29 @@ export default function Login() {
                   Forgot your password?
                   </a>
                   </div> */}
-                  <Input
-                    id="password"
-                    type="password"
-                    required
-                    name="password"
-                  />
-                </div>
+                <Input id="password" type="password" required name="password" />
               </div>
-              <CardFooter className="flex-col gap-2 mt-6">
-                {state?.error && <p>{state.error}</p>}
-                {message === "check-email" ? (
-                  <p>Check email for verification before logging in</p>
-                ) : null}
-                <Button type="submit" className="w-full">
-                  {isPending ? "Logging in..." : "Login"}
-                </Button>
-              </CardFooter>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
+            </div>
+            <CardFooter className="flex-col gap-2 mt-6">
+              {state?.error && <p>{state.error}</p>}
+              {message === "check-email" ? (
+                <p>Check email for verification before logging in</p>
+              ) : null}
+              <Button type="submit" className="w-full">
+                {isPending ? "Logging in..." : "Login"}
+              </Button>
+            </CardFooter>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default function Login() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
     </Suspense>
   );
 }
